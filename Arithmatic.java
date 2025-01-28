13b60a83284662079e25aca3500d74c2e194a375
Pranali Rameshwar paikekari <pranalipaikekari07@gmail.com>
2025-01-28 20:29:20 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,17 @@
+class Arithmatic
+{
+public static void main(String args[])
+{
+int a=2,b=4,c,d,e,f,q;
+q=a+b;
+c=a%b;
+d=a/b;
+e=a*b;
+f=a-b;
+System.out.println("add:"+q);
+System.out.println("mod:"+c);
+System.out.println("div:"+d);
+System.out.println("multiply:"+e);
+System.out.println("substract:"+f);
+}
+}
