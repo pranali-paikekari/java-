3f59066d3d561af1543e033b33812bcdb563eb50
Pranali Rameshwar paikekari <pranalipaikekari07@gmail.com>
2025-01-28 20:30:50 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,19 @@
+class Assignment 
+{
+public static void main(String args[])
+{
+int a=2,b=4;
+a+=b;
+b-=a;
+a/=b;
+a*=b;
+b%=a;
+System.out.println("add:"+a);
+System.out.println("sub:"+b);
+System.out.println("div:"+a);
+System.out.println("mul:"+a);
+System.out.println("mod:"+b);
+
+
+}
+}
