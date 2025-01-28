6cd30868c1163a31216245b20fb01b2d346d7e7c
Pranali Rameshwar paikekari <pranalipaikekari07@gmail.com>
2025-01-28 20:30:32 +0530

    Add files via upload

---
+++ Array2d.java
@@ -0,0 +1,16 @@
+class Array2d
+{
+public static void main(String args[])
+{
+int a[][]={{12,3,4},{3,5,7}};
+int i,j;
+for(i=0;i<2;i++)
+{
+for(j=0;j<3;j++)
+{
+  System.out.print(a[i][j] + " ");
+            System.out.println();
+}
+}
+}
+}
