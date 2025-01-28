992093ee9432779c26bf8f8ca5835cd13633d214
Pranali Rameshwar paikekari <pranalipaikekari07@gmail.com>
2025-01-28 20:31:35 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,10 @@
+class Conditional
+{
+public static void main(String args[])
+{
+int a=2,b=7;
+
+System.out.println((a==b)?"both are equal":"not equal");
+System.out.println((a>b)?"a is greater than b ":"a is not  greater");
+}
+}
