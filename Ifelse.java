bf60f3b5f5df83aa569187cbfedb578d078affb2
Pranali Rameshwar paikekari <pranalipaikekari07@gmail.com>
2025-01-28 20:33:56 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,17 @@
+class Ifelse
+{
+public static void main(String args[])
+{
+int a=4,b=6;
+if(a>b)
+{
+
+System.out.println("b is greater");
+}
+else
+{
+System.out.println("a is greater");
+}
+
+}
+}
